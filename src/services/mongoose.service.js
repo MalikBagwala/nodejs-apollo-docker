927789823c105a20922095d6488ca34646a04ade
Malik <malikeditz@gmail.com>
2020-01-12 10:33:42 +0530

    added a few required packages and docker-compose.yml

---
+++ src/services/mongoose.service.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://mongo-database:27017/nodejs-apollo-docker", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(c => console.log("MONGO CONNECTED ##"))
+  .catch(err => console.error(err));
